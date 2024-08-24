ede96863d99da81f70fac908e412044aba42145e
lawrence3288 <lawrence3288lewis@outlook.com>
2024-08-24 11:17:07 +0530

    Create src_components_Header.js

---
+++ src_components_Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className='header'>
+      <h1>Florida Blue Expense Tracker</h1>
+    </header>
+  );
+}
+
+export default Header;
